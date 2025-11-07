575c7e19d810c18bdaf47278a71c482f50573058
dev-dark25 <283648851@qq.com>
2025-11-07 16:23:08 +0800

    Add files via upload

--- deepseek_vue/src/App.vue
+++ deepseek_vue/src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <header>
-    <h2>DeepSeek</h2>
-    <div>
-      <el-radio-group v-model="model">
+    <h2>AI model</h2>
+    <div style="margin-top: 1rem;">
+      <el-select v-model="type" placeholder="Select" style="width: 120px" @change="typeChange()">
+        <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+      </el-select>
+      <!-- <el-radio-group v-model="model">
         <el-radio value="local">local</el-radio>
         <el-radio value="deepseek-reasoner">deepseek r1</el-radio>
         <el-radio value="deepseek-chat">deepseek v3</el-radio>
-      </el-radio-group>
+      </el-radio-group> -->
+      <el-select v-show="flag" v-model="model" placeholder="Select" style="width: 120px;margin-left: 1rem;">
+        <el-option v-for="item in dsModels" :key="item.value" :label="item.label" :value="item.value" />
+      </el-select>
+      <el-select v-show="!flag" v-model="model" placeholder="Select" style="width: 120px;margin-left: 1rem;">
+        <el-option v-for="item in qwModels" :key="item.value" :label="item.label" :value="item.value" />
+      </el-select>
     </div>
+
     <div class="wrapper" style="margin-top: 1rem">
       <el-input v-model="input" style="width: 500px" placeholder="Please input" @change="inputChange()" />
     </div>
@@ -25,8 +35,48 @@ export default {
   data() {
     return {
       input: 'input',
-      model: 'deepseek-reasoner',
-      output: 'output'
+      model: '',
+      output: 'output',
+      type: 'DeepSeek',
+      flag: true,
+      options: [
+        {
+          value: 'DeepSeek',
+          label: 'DeepSeek',
+        },
+        {
+          value: 'QWen',
+          label: 'QWen',
+        }
+      ],
+      dsModels: [
+        {
+          value: 'local',
+          label: 'local',
+        },
+        {
+          value: 'deepseek-reasoner',
+          label: 'deepseek r1',
+        },
+        {
+          value: 'deepseek-chat',
+          label: 'deepseek v3',
+        }
+      ],
+      qwModels: [
+        {
+          value: 'local',
+          label: 'local',
+        },
+        {
+          value: 'qwen3-max',
+          label: 'qwen3 max',
+        },
+        {
+          value: 'qwen-plus',
+          label: 'qwen plus',
+        }
+      ],
     }
   },
   created() {
@@ -36,6 +86,11 @@ export default {
     this.input = ''
   },
   methods: {
+    typeChange() {
+      this.flag = !this.flag;
+      this.model = '';
+    },
+
     async inputChange() {
       const css1 = 'color: #409eff;text-align: right;';
       const css2 = 'color: #67c23a;text-align: left;';
@@ -43,6 +98,8 @@ export default {
       newElement.setAttribute('style', css1);
       newElement.textContent = this.input;
       this.$refs.container.appendChild(newElement);
+      this.$refs.container.scrollTop = this.$refs.container.scrollHeight;
+      this.input = '';
 
       // css加载中
       const css3 = 'border: 5px solid #f3f3f3;border-top: 5px solid #3498db;border-radius: 50%;width: 25px;height: 25px;animation: spin 2s linear infinite;';
@@ -55,6 +112,7 @@ export default {
 
       const req = {
         input: this.input,
+        type: this.type,
         model: this.model
       }
       const res = await call(req);
@@ -67,7 +125,6 @@ export default {
       this.$refs.container.removeChild(newElement1);
       this.$refs.container.appendChild(newElement2);
       this.$refs.container.scrollTop = this.$refs.container.scrollHeight;
-      this.input = '';
     }
   }
 }
